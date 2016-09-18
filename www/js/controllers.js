01c0e97df021692736d9bb921ab99144914b0604
fedeakd <soiifede18@gmail.com>
2016-09-18 04:14:42 -0300

    Sonido :D

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic', 'ngCordova'])
 
 .controller('presentacionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -8,20 +8,47 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('pianoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('pianoCtrl', ['$scope', '$stateParams','$ionicPlatform', '$timeout',  '$cordovaNativeAudio', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams,$ionicPlatform, $timeout, $cordovaNativeAudio) {
+	var vm = this;
+	$scope.siLlego="no";
+	$ionicPlatform.ready(function() {
+
+        // all calls to $cordovaNativeAudio return promises
+        try{
+        	$cordovaNativeAudio.preloadSimple('caballo', 'sonidos/Caballo.mp3');
+        	$cordovaNativeAudio.preloadSimple('puerco', 'sonidos/Cerdo.mp3');
+        	$cordovaNativeAudio.preloadSimple('gallo', 'sonidos/Gallo.mp3');
+        	$cordovaNativeAudio.preloadSimple('gato', 'sonidos/Gato.mp3');
+        	$cordovaNativeAudio.preloadSimple('lechuza', 'sonidos/Lechuza.mp3');
+        	$cordovaNativeAudio.preloadSimple('leon', 'sonidos/Leon.mp3');
+        }
+        catch( e){
+        	alert("No estas en un celular");
+        }
+    });
+
+	$scope.play = function(sound) {
+		$scope.siLlego="si";
+		$cordovaNativeAudio.play(sound);
+	};
 
 
 }])
 
-.controller('usuariosCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('usuariosCtrl', ['$scope', '$stateParams','$state', '$cordovaFile',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$state) {
+function ($scope, $stateParams,$state,$cordovaFile) {
 
-
+$cordovaFile.writeFile(window.cordova.file.dataDirectory, "file.txt", "text", true)
+  .then(function (success) {
+    // success
+  }, function (error) {
+    // error
+  });
 }])
 
 .controller('loginCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
