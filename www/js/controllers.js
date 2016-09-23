725e34d1007cf8412206006f595ae546b7dfa96e
fedeakd <soiifede18@gmail.com>
2016-09-23 00:12:53 -0300

    listo

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,7 @@ function ($scope, $stateParams) {
 }])
 
 .controller('pianoCtrl', 
-  function ($scope, $stateParams,$ionicPlatform, $timeout, $cordovaNativeAudio,Informacion,$cordovaFile,$cordovaVibration) {
+  function ($scope, $stateParams,$ionicPlatform, $timeout, $cordovaNativeAudio,Informacion,$cordovaFile,$cordovaVibration,$ionicPopup) {
    var vm = this;
    $scope.yaguardo=false;
    $ionicPlatform.ready(function() {
@@ -29,8 +29,8 @@ function ($scope, $stateParams) {
 
    $scope.play = function(sound) {
      $cordovaVibration.vibrate(500);
-    if(Informacion.artista.sonidos.length ==6){
-       
+     if(Informacion.artista.sonidos.length ==6){
+
       Informacion.artista.sonidos.unshift(sound); 
       Informacion.artista.sonidos.pop();   
     }
@@ -65,7 +65,11 @@ function ($scope, $stateParams) {
         $ionicPlatform.ready(function() {
           $cordovaFile.writeFile(cordova.file.externalRootDirectory, "matrix123.txt", JSON.stringify(arrayUsuarios), true)
           .then(function (success) {
+            $ionicPopup.alert({
+              title: 'Guardo!',
+              template: 'Se ah guardado los cambios !!'
 
+            }); 
           }, function (error) {
           });
 
@@ -105,11 +109,11 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams,$state,$cordovaFile,$ionicPlatform,$cordovaNativeAudio,$cordovaVibration) {
 
-    $scope.arrayUsuarios=[];
+  $scope.arrayUsuarios=[];
  // $scope.arrayUsuarios=[{jugador:'fede',estado:'none'  ,apellido:'none',sonidos:['caballo','gato','gato']},
  // {jugador:'fede' ,estado:'none',apellido:'santamaria',sonidos:['caballo','gallo','gato']}];
  // $scope.arrayUsuarios.push({jugador:'josefa',apellido:'nidea',estado:"none",sonidos:['caballo','gallo','gato','puerco','lechuza','leon']});
-  $ionicPlatform.ready(function() {
+ $ionicPlatform.ready(function() {
 
         // all calls to $cordovaNativeAudio return promises
         try{
@@ -125,41 +129,41 @@ function ($scope, $stateParams,$state,$cordovaFile,$ionicPlatform,$cordovaNative
         }
       });
 
-  $scope.play = function(sound) {
-    try{
-      $cordovaNativeAudio.play(sound);
-      $cordovaVibration.vibrate(500);
-    }
-    catch(e){
-      console.log("No estas en un celular");
-    }
-  }
-  $scope.MostrarContenedor=function(num){
-    console.log(num);
-
-    if($scope.arrayUsuarios[num].estado==='none'){
-      $scope.arrayUsuarios[num].estado="show";
-    }
-    else{
-      $scope.arrayUsuarios[num].estado="none";
-    }
-
-  }
-
+ $scope.play = function(sound) {
   try{
+    $cordovaNativeAudio.play(sound);
+    $cordovaVibration.vibrate(500);
+  }
+  catch(e){
+    console.log("No estas en un celular");
+  }
+}
+$scope.MostrarContenedor=function(num){
+  console.log(num);
 
-    $cordovaFile.readAsText(cordova.file.externalRootDirectory, "matrix123.txt")
-    .then(function(success) {
-      $scope.arrayUsuarios=JSON.parse(success);
+  if($scope.arrayUsuarios[num].estado==='none'){
+    $scope.arrayUsuarios[num].estado="show";
+  }
+  else{
+    $scope.arrayUsuarios[num].estado="none";
+  }
+
+}
+
+try{
+
+  $cordovaFile.readAsText(cordova.file.externalRootDirectory, "matrix123.txt")
+  .then(function(success) {
+    $scope.arrayUsuarios=JSON.parse(success);
 
 
-    }, function(error){
-      alert('didn\'t find the file: ' + error.code);
-    })}
-    catch(e){
-      console.log("no es celu");
-    }
-  })
+  }, function(error){
+    alert('didn\'t find the file: ' + error.code);
+  })}
+  catch(e){
+    console.log("no es celu");
+  }
+})
 
 .controller('loginCtrl', ['$scope','$stateParams','$state', 'Informacion', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
